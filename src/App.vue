af2828b286ce3ea6e987f319af0cf20e4436000a
patri test <fatupatricia2004@gmail.com>
2021-01-21 17:40:03 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <todo-list list-name="My to-dos" />
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+};
+
+
+
+</script>
+
